ec680ba0238a329ef62f4507a5c84141da14dc28
isaivani95 <144344342+isaivani95@users.noreply.github.com>
2023-09-12 15:59:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# Factorial of a number using recursion
-
-def recur_factorial(n):
-   if n == 1:
-       return n
-   else:
-       return n*recur_factorial(n-1)
-
-num = 7
-
-# check if the number is negative
-if num < 0:
-   print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-   print("The factorial of 0 is 1")
+def isleapyear(year):
+  if(year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    return True
+  else:
+    return False
+   
+year=2013
+if isleapyear(year):
+  print ('{} is a leap year.'. format(year))
 else:
-   print("The factorial of", num, "is", recur_factorial(num));
+  print('{} is not a leap year.'.format(year))
